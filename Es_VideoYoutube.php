33319b9d0144193847e8b4c8d0c2c33369e844e7
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-11 09:49:16 +0200

    Aggiuno array associativo dei film e funzione inserisciVideoYoutube

--- Es_VideoYoutube.php
+++ Es_VideoYoutube.php
@@ -7,6 +7,16 @@
 </head>
 <body>
     <?php
+    $v1 = array("titolo" => "A MILANO CON 5000 CAVALLI | #IGAC 19", "idVideo" => "9YPiZ8Qu-os");
+    $v2 = array("titolo" => "ACCADONO COSE EP:17", "idVideo" => "-qcgdussKDQ");
+    $v3 = array("titolo" => "I PIU' SCORRETTI CAMPIONI di Formula 1", "idVideo" => "MpPuEj7RuQs");
+    $listaVideo = array($v1, $v2, $v3);
+    inserisciVideoYoutube($listaVideo[0], 50, 250);
+
+    function inserisciVideoYoutube($v, $larPerc, $altPx){
+        echo "<h2>$v[titolo]</h2>";
+        echo "<iframe src='https://www.youtube.com/watch?v=" . $v["idVideo"] . "' height='$altPx %' width='$larPerc px' ></iframe>";
+    }
     ?>
 </body>
 </html>
